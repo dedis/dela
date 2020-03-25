77ee16f4d70333f8cf8aaa365212409bbfaad0fd
Gaylor Bosson <gaylor.bosson@gmail.com>
2020-03-25 08:23:50 +0100

    Address nkcr's comments

--- blockchain/viewchange/mod.go
+++ blockchain/viewchange/mod.go
@@ -9,9 +9,9 @@ import (
 // propose a block as the leader. It is also responsible for verifying the
 // integrity of the players of the chain.
 type ViewChange interface {
-	// Wait returns a non-nil error when the node is allowed to make the
-	// proposal. It will also return the authorized list of players that must be
-	// used so that the Verify function returns nil.
+	// Wait returns a nil error when the player is allowed to propose the block.
+	// It will also return the authorized list of players that must be used so
+	// that the Verify function returns nil.
 	//
 	// Note: the implementation of the returned mino.Players interface must be
 	// preserved.
