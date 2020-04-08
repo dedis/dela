3444f073ff4a96d82ae6cd3eb5bc6379ea77231d
Noémien Kocher <nkcr.je@gmail.com>
2020-04-08 08:26:56 +0200

    Revert "Reverts past refactoring"
    This reverts commit 7960d7b1bcee8eaaad7adfd2ce686e6bc4bbf3b7.

--- internal/mcheck/mod.go
+++ internal/mcheck/mod.go
@@ -6,7 +6,6 @@ package main
 // `go build && go vet -vettool=./check -commentLen ./...`
 
 import (
-	"go/ast"
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
@@ -30,32 +29,25 @@ func main() {
 
 // run parses all the comments in ast.File
 func run(pass *analysis.Pass) (interface{}, error) {
+fileLoop:
 	for _, file := range pass.Files {
-		ast.Inspect(file, func(node ast.Node) bool {
-			switch x := node.(type) {
-			case *ast.File:
-				comments := x.Comments
-				first := true
-				for _, cg := range comments {
-					for _, c := range cg.List {
-						if first && strings.HasPrefix(c.Text, "// Code generated") {
-							return false
-						}
-						lines := strings.Split(c.Text, "\n")
-						for _, line := range lines {
-							if len(line) > MaxLen {
-								pass.Reportf(c.Pos(), "Comment too long: %s (%d)",
-									line, len(line))
-							}
-						}
-						first = false
+		isFirst := true
+		for _, cg := range file.Comments {
+			for _, c := range cg.List {
+				if isFirst && strings.HasPrefix(c.Text, "// Code generated") {
+					continue fileLoop
+				}
+				// in case of /* */ comment there might be multiple lines
+				lines := strings.Split(c.Text, "\n")
+				for _, line := range lines {
+					if len(line) > MaxLen {
+						pass.Reportf(c.Pos(), "Comment too long: %s (%d)",
+							line, len(line))
 					}
 				}
-				return false
-			default:
+				isFirst = false
 			}
-			return false
-		})
+		}
 	}
 	return nil, nil
 }
