99f2cef03b5cf6ac7e493202d922a04c0aae1faa
Noémien Kocher <nkcr.je@gmail.com>
2020-04-08 11:05:25 +0200

    Removes unecessary test

--- internal/mcheck/mod_test.go
+++ internal/mcheck/mod_test.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"os"
 	"testing"
 
 	"golang.org/x/tools/go/analysis/analysistest"
 )
-
-func TestMain(m *testing.M) {
-	os.Exit(m.Run())
-}
 
 func TestCommentLen(t *testing.T) {
 	analysistest.Run(t, analysistest.TestData(), commentAnalyzer)
