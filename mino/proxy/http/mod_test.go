e2223e0e5ac16e3c9a76270305209013f07f766e
Noémien Kocher <nkcr.je@gmail.com>
2020-07-17 12:03:55 +0200

    Updates the test to avoid address collision

--- mino/proxy/http/mod_test.go
+++ mino/proxy/http/mod_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestHTTP_Listen(t *testing.T) {
-	proxy := NewHTTP("127.0.0.1:2000")
+	proxy := NewHTTP("127.0.0.1:2010")
 	go proxy.Listen()
 	time.Sleep(200 * time.Millisecond)
 
@@ -18,7 +18,7 @@ func TestHTTP_Listen(t *testing.T) {
 
 	proxy.RegisterHandler("/fake", fakeHandler)
 
-	res, err := http.Get("http://127.0.0.1:2000/fake")
+	res, err := http.Get("http://127.0.0.1:2010/fake")
 	require.NoError(t, err)
 
 	output, err := ioutil.ReadAll(res.Body)
