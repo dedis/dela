e20139681e687b7f60b9e34750f3eb41957529f7
Gaylor Bosson <gaylor.bosson@epfl.ch>
2020-09-16 13:42:43 +0200

    Access: unit tests

--- core/access/credentials.go
+++ core/access/credentials.go
@@ -2,12 +2,16 @@ package access
 
 import "fmt"
 
+// ContractCredentials defines the credentials for a contract. It contains the
+// name of the contract and an associated command.
 type ContractCredentials struct {
 	id       []byte
 	contract string
 	command  string
 }
 
+// NewContractCreds creates new credentials from the associated identifier, the
+// name of the contract and its command.
 func NewContractCreds(id []byte, contract, command string) ContractCredentials {
 	return ContractCredentials{
 		id:       id,
@@ -16,10 +20,12 @@ func NewContractCreds(id []byte, contract, command string) ContractCredentials {
 	}
 }
 
+// GetID returns the identifier for the credentials.
 func (cc ContractCredentials) GetID() []byte {
 	return append([]byte{}, cc.id...)
 }
 
+// GetRule returns the scope of the credentials.
 func (cc ContractCredentials) GetRule() string {
 	return fmt.Sprintf("%s:%s", cc.contract, cc.command)
 }
--- core/access/mod.go
+++ core/access/mod.go
@@ -17,13 +17,37 @@ type Identity interface {
 	Equal(other interface{}) bool
 }
 
+// Credentials is an abstraction of an entity that allows one or several
+// identities to access a given scope.
+//
+// As an example, the identifier is the username of a username/password pair. It
+// defines the component to compare against. Then the password is the list of
+// identities, verified beforehands, that will match, or won't, match to the
+// identifier underlying permissions. The rule defines which scope should be
+// verified so that the permissions can hold multiple of thoses.
+//
+//   []byte{0xde, 0xad, 0xbe, 0xef} -> {
+//     "myContract:sayHello": ["Alice", "Bob"],
+//     "myContract:sayBye": ["Bob"],
+//   }
+//
+// The example above shows an access control for the contract "myContract" that
+// is allowing two commands "sayHello" and "sayBye". Alice and Bob can say
+// hello, but only Bob is allow to say bye. Alice can prove that she's allowed
+// by providing credentials with the identifier 0xdeadbeef.
 type Credentials interface {
 	GetID() []byte
 	GetRule() string
 }
 
+// Service is an access control service that can read the storage to find
+// permissions associated to the credentials, or update existing ones.
 type Service interface {
+	// Match returns nil if the credentials can be matched to the group of
+	// identities.
 	Match(store store.Readable, creds Credentials, idents ...Identity) error
 
+	// Grant updates the store so that the group of identities will match the
+	// credentials.
 	Grant(store store.Snapshot, creds Credentials, idents ...Identity) error
 }
