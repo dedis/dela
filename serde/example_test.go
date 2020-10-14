90dd31c71fd07afa453587a5e60176237b4c3cd8
Gaylor Bosson <gaylor.bosson@epfl.ch>
2020-10-14 08:48:04 +0200

    Serde: add XML to the example
    This demonstrates how different formats can be used by simply switching
    the context.

--- serde/example_test.go
+++ serde/example_test.go
@@ -7,36 +7,48 @@ import (
 	"go.dedis.ch/dela/serde"
 	"go.dedis.ch/dela/serde/json"
 	"go.dedis.ch/dela/serde/registry"
+	"go.dedis.ch/dela/serde/xml"
 )
 
-func ExampleMessage_Serialize_json() {
+func ExampleMessage_Serialize() {
 	// Register a JSON format engine for the message type.
 	exampleRegistry.Register(serde.FormatJSON, exampleJSONFormat{})
+	// Register a Gob format engine for the message type.
+	exampleRegistry.Register(serde.FormatXML, exampleXMLFormat{})
 
 	msg := exampleMessage{
 		value: 42,
 	}
 
-	ctx := json.NewContext()
-
-	data, err := msg.Serialize(ctx)
+	data, err := msg.Serialize(json.NewContext())
 	if err != nil {
 		panic("serialization failed: " + err.Error())
 	}
 
-	fmt.Println(string(data))
+	fmt.Println("JSON", string(data))
 
-	// Output: {"value":42}
+	data, err = msg.Serialize(xml.NewContext())
+	if err != nil {
+		panic("serialization failed: " + err.Error())
+	}
+
+	fmt.Println("XML", string(data))
+
+	// Output: JSON {"value":42}
+	// XML <exampleMessageXML><value>42</value></exampleMessageXML>
 }
 
-func ExampleFactory_Deserialize_json() {
-	ctx := json.NewContext()
-
-	data := []byte(`{"Value":42}`)
-
+func ExampleFactory_Deserialize() {
 	factory := exampleFactory{}
 
-	msg, err := factory.Deserialize(ctx, data)
+	msg, err := factory.Deserialize(json.NewContext(), []byte(`{"Value":42}`))
+	if err != nil {
+		panic("deserialization failed: " + err.Error())
+	}
+
+	fmt.Printf("%+v\n", msg)
+
+	msg, err = factory.Deserialize(xml.NewContext(), []byte("<exampleMessageXML><value>12</value></exampleMessageXML>"))
 	if err != nil {
 		panic("deserialization failed: " + err.Error())
 	}
@@ -44,6 +56,7 @@ func ExampleFactory_Deserialize_json() {
 	fmt.Printf("%+v", msg)
 
 	// Output: {value:42}
+	// {value:12}
 }
 
 var exampleRegistry = registry.NewSimpleRegistry()
@@ -53,6 +66,19 @@ var exampleRegistry = registry.NewSimpleRegistry()
 // - implements serde.Message
 type exampleMessage struct {
 	value int
+}
+
+// Serialize implements serde.Message. It returns the JSON serialization of a
+// message example.
+func (m exampleMessage) Serialize(ctx serde.Context) ([]byte, error) {
+	format := exampleRegistry.Get(ctx.GetFormat())
+
+	data, err := format.Encode(ctx, m)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
 
 // exampleFactory is a example of a message factory.
@@ -82,19 +108,6 @@ type exampleMessageJSON struct {
 	Value int `json:"value"`
 }
 
-// Serialize implements serde.Message. It returns the JSON serialization of a
-// message example.
-func (m exampleMessage) Serialize(ctx serde.Context) ([]byte, error) {
-	format := exampleRegistry.Get(ctx.GetFormat())
-
-	data, err := format.Encode(ctx, m)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
-
 // exampleJSONFormat is an example of a format to serialize a message example
 // using a JSON encoding.
 //
@@ -116,9 +129,52 @@ func (exampleJSONFormat) Encode(ctx serde.Context, msg serde.Message) ([]byte, e
 	return ctx.Marshal(m)
 }
 
-// Decode implements serde.FormatEngine. It is not implemented in this example.
+// Decode implements serde.FormatEngine. It populates a message example if
+// appropritate, otherwise it returns an error.
 func (exampleJSONFormat) Decode(ctx serde.Context, data []byte) (serde.Message, error) {
 	var m exampleMessageJSON
+	err := ctx.Unmarshal(data, &m)
+	if err != nil {
+		return nil, err
+	}
+
+	msg := exampleMessage{
+		value: m.Value,
+	}
+
+	return msg, nil
+}
+
+// exampleMessageXML is an XML message for a message example.
+type exampleMessageXML struct {
+	Value int `xml:"value"`
+}
+
+// exampleXMLFormat is an example if a format to serialize a message example
+// using the XML encoding.
+//
+// - implements serde.FormatEngine
+type exampleXMLFormat struct{}
+
+// Encode implements serde.FormatEngine. It formats the message to comply with
+// the XML encoding and marshal it.
+func (exampleXMLFormat) Encode(ctx serde.Context, msg serde.Message) ([]byte, error) {
+	example, ok := msg.(exampleMessage)
+	if !ok {
+		return nil, errors.New("unsupported message")
+	}
+
+	m := exampleMessageXML{
+		Value: example.value,
+	}
+
+	return ctx.Marshal(m)
+}
+
+// Decode implements serde.FormatEngine. It populates a message example if
+// appropritate, otherwise it returns an error.
+func (exampleXMLFormat) Decode(ctx serde.Context, data []byte) (serde.Message, error) {
+	var m exampleMessageXML
 	err := ctx.Unmarshal(data, &m)
 	if err != nil {
 		return nil, err
